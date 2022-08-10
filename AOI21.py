5504b518ab9ca9d0901da03edd948fa6e91f5cc0
Asira <ashkanr65@yahoo.com>
2022-08-10 11:49:43 +0200

    Small changes done

--- AOI21.py
+++ AOI21.py
@@ -702,7 +702,7 @@ class AOI21_v2(pya.PCellDeclarationHelper):
         ("I_NA1", 0.001, 5).move((x1- gate_edge)/ dbu, -Top_Path - 25 / dbu)
     self.cell.shapes(txt).insert(iTregion)
 
-    # (level, x, y, w_i, n_i, l_i, bg, Load, In_Con, overlap_left, overlap_rigth, out)3
+    # (level, x, y, w_i, n_i, l_i, bg, Load, In_Con, overlap_left, overlap_rigth, out)2
     x2 = x1 + 5*d_x_101
     self.transistor(2, x2, y, w_d, self.n_d, self.l_d, True, False, 101, d_x_101, 2*d_x_101, 0)
     #gates name
@@ -710,7 +710,7 @@ class AOI21_v2(pya.PCellDeclarationHelper):
         ("I_NO2", 0.001, 5).move((x2- gate_edge)/ dbu, -Top_Path - 25 / dbu)
     self.cell.shapes(txt).insert(iTregion)
 
-    # (level, x, y, w_i, n_i, l_i, bg, Load, In_Con, overlap_left, overlap_rigth, out)4
+    # (level, x, y, w_i, n_i, l_i, bg, Load, In_Con, overlap_left, overlap_rigth, out)3
     x3 = x2 + 5*l_x
     self.transistor(3, x3, y, w_d, p * self.n_d, self.l_l, True, True, 101, l_x, 0, 1)
     #gates name
