21748ef76b3e7b088893083427a9dfa66353bd6a
Asira <ashkanr65@yahoo.com>
2023-03-08 16:51:08 +0100

    Optimized, Commented

--- via.py
+++ via.py
@@ -17,27 +17,27 @@ class Via(pya.PCellDeclarationHelper):
         super(Via, self).__init__()
 
         # Declare the parameters
-        self.param("r", self.TypeDouble, "Via radius", default=1.25)
-        self.param("ov", self.TypeDouble, "Overlap", default=2.5)
-        self.param("f_lay", self.TypeDouble, "First layer", default=1)
+        self.param("radius", self.TypeDouble, "Via radius", default=1.25)   # declaring the radius parameter (default is 1.25)
+        self.param("overlap", self.TypeDouble, "Overlap", default=2.5)     # declaring the overlap parameter (default is 2.5)
+        self.param("first_layer", self.TypeDouble, "First layer", default=1)   # declaring the first layer parameter (default is 1)
 
     def display_text_impl(self):
         """
         Provide a descriptive text for the cell.
         """
         return "Via"
-    
+
     def coerce_parameters_impl(self):
         # Check if layer has been selected correctly   
-        if self.f_lay not in [1, 3, 4]:
+        if self.first_layer not in [1, 3, 4]:    # checks if first layer is either 1, 3, or 4
             raise Exception("Incorrect layer has been selected")
         
         # Check radius   
-        if self.r < 1.25:
+        if self.radius < 1.25:      # checks if radius is less than 1.25
             raise Exception("You cannot make radius smaller than 1.25")
         
         # Check overlap
-        if self.ov < 2.5:
+        if self.overlap < 2.5:      # checks if overlap is less than 2.5
             raise Exception("Overlap cannot be less than 2.5")
 
     def can_create_from_shape_impl(self):
@@ -47,24 +47,22 @@ class Via(pya.PCellDeclarationHelper):
         """
         return self.shape.is_box() or self.shape.is_polygon() or self.shape.is_path()
 
+    
     def produce_impl(self):
         """
         Create the Via PCell.
         """
         nr = 128  # Number of points in a circle
 
-        # Define layer names
-        layers = [self.layout.layer(i, 0) for i in [0, 1, 2, 3, 4, 6, 7, 9]]
-        # Unpack layers into variables
-        txt, bm, bl, sd, gm, pv, gc, pv2 = layers
-        dbu = self.layout.dbu
-        r = self.r / dbu
-        ov = self.ov / dbu
-
-        # Adjust layer numbers if greater than 5
-        self.f_lay -= self.f_lay > 5
-        # Create via region
-        via_box = pya.Region(pya.Box(-r, -r, r, r)).rounded_corners(0, 2*r, nr)
+        # Define layer names and layes using list comprehension
+        txt, bm, bl, sd, gm, pv, gc, pv2 = [self.layout.layer(i, 0) for i in range(10) if i in [0, 1, 2, 3, 4, 6, 7, 9]]
+        dbu = self.layout.dbu       # gets the database unit
+        radius = self.radius / dbu   # calculates the radius in terms of DBU
+        overlap = self.overlap / dbu   # calculates the ovelap in terms of DBU
+        
+        # Create via region using pya.Region()
+        via_box = pya.Region(pya.Box(-radius, -radius, radius, radius)).rounded_corners(0, 2*radius, nr)
+        # Insert shapes into specific layers
         self.cell.shapes(pv).insert(via_box)
-        self.cell.shapes(self.f_lay).insert(via_box.sized(ov))
-        self.cell.shapes(gc).insert(via_box.sized(ov))
+        self.cell.shapes(self.first_layer).insert(via_box.sized(overlap))
+        self.cell.shapes(gc).insert(via_box.sized(overlap))
